d9bfac506dda3aaee9294bd3048d40f67a7eaafd
droc101 <37421449+droc101@users.noreply.github.com>
2026-05-05 14:16:35 -0500

    navigate from home to setup to play

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,8 +1,13 @@
+import {useNavigate} from "react-router";
 
 const Home = () => {
+
+    const nav = useNavigate();
+
     return (
         <>
             <h1>Home</h1>
+            <button className="btn btn-primary" onClick={() => nav("/setup")}>Play</button>
         </>
     )
 }
--- src/components/pages/Setup.tsx
+++ src/components/pages/Setup.tsx
@@ -1,8 +1,14 @@
+import {useNavigate} from "react-router";
 
 const Setup = () => {
+
+    const nav = useNavigate();
+
     return (
         <>
             <h1>Setup</h1>
+            <button className="btn btn-soft btn-warning" onClick={() => nav("/")}>Cancel</button>
+            <button className="btn btn-primary" onClick={() => nav("/play")}>Play</button>
         </>
     )
 }
